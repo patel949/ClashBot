e7574178d97b47c3ce1c26407e14a45f62382577
patel949 <patel949@purdue.edu>
2021-08-24 11:15:24 -0400

    small updates to Player and Command files

--- lib/src/main/java/commands/Player.java
+++ lib/src/main/java/commands/Player.java
@@ -21,7 +21,7 @@ public class Player extends Command {
 	//get a list of "hooks"
 	@Override
 	public String[] getHooks() {
-		String[] hooks = {"manage"};
+		String[] hooks = {"player"};
 		return hooks;
 	}
 	
@@ -34,9 +34,24 @@ public class Player extends Command {
 		}
 		
 		if (command[1].equalsIgnoreCase("Promote")) {
+			//First, get the level of the current player.
+			boolean promoteToLeader = Command.permissable(e.getMember(), Command.userPerms.ADMIN); //Admin and Leader are nearly identical
+			boolean promoteToColeader = promoteToLeader || Command.permissable(e.getMember(), Command.userPerms.COLEADER);
+			boolean promoteToElder = promoteToColeader || Command.permissable(e.getMember(), Command.userPerms.ELDER);
+			
+			//If you can't even promote someone to elder, quit now.
+			if (!promoteToElder)
+				return;
+			
+			//For each mentioned player, either promote or fail to promote.
 			List<Member> players = e.getMessage().getMentionedMembers();
 			for (Member player : players) {
-				//Check the current 
+				//Check the current role of the player.
+				userPerms p = Command.getHighestRole(player);
+				if (p == userPerms.GUEST) {
+					e.getGuild().addRoleToMember(player, e.getGuild().getRolesByName("",false).get(0));
+				}
+					
 			}
 		}
 		
