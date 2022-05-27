cc26a55c3d047a9942a1c661d8662311ff2eee26
Joseph Patel <patel949@purdue.edu>
2022-05-26 21:32:44 -0400

    F1 F2 Subscribe/Unsubscribe to a clan
    implemented F1 and F2, you can subscribe/unsubscribe from clans as a Discord.

--- lib/src/main/java/org/jprojects/lib/commands/Subscribe.java
+++ lib/src/main/java/org/jprojects/lib/commands/Subscribe.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.jprojects.lib.ServerData;
 import org.jprojects.lib.commands.Command.userPerms;
 import org.jprojects.lib.database.DiscordToClashDF;
+import org.jprojects.scapi.ScapiClanAF;
 import org.jprojects.scapi.ScapiPlayerAF;
 
 import net.dv8tion.jda.api.entities.MessageChannel;
@@ -80,7 +81,26 @@ public class Subscribe extends Command {
 	}
 	
 	private void executeForClan(MessageReceivedEvent e, String[] command) {
+		if ( !Command.permissable(e.getMessage().getMember(), userPerms.LEADER) )  {
+			e.getChannel().sendMessage("You don't have permission to subscribe the Discord to a new clan.").queue();
+			return;
+		}
 		
+		String discordID = e.getGuild().getId();
+		String clashID = command[command.length-1];
+		String clashName = ScapiClanAF.getInstance().getClanNameFromTag(clashID);
+		
+		if (clashName == null) {
+			e.getChannel().sendMessage("The clan tag " + clashID + " appears to be invalid.").queue();
+			return;
+		}
+		
+		//well, we have permission. we have a valid clan. we have a discord. log it.
+		boolean success = DiscordToClashDF.getDiscordtoClashDF().AddClanToDiscordServer(discordID, clashID);
+		if (success)
+			e.getChannel().sendMessage("Great! This discord server is now subscribed to notifications for the clan '" + clashName + "'").queue();
+		else
+			e.getChannel().sendMessage("Uh-oh, something went wrong, but I'm not quite sure what. If this happens again, contact the dev at dev@jprojects.org with the command you tried to use.").queue();
 	}
 	
 	@Override
